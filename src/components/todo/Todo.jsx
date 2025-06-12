0e90162179af08c488c148094ae1acd3e8e5d8ab
Silvija96 <silvija.barkauskaite064@gmail.com>
2025-06-12 14:58:33 +0300

    functions

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,10 +1,48 @@
+import { useRef, useState } from 'react';
 import style from './Todo.module.css';
+import { randomNumber } from '../../lib/randomNumber';
 
 export function Todo() {
+    const [todoList, setTodoList] = useState([]);
+    const [text, setText] = useState('');
+    const inputRef = useRef();
+
+    function handleInputChange(e) {
+        setText(e.target.value);
+    }
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        if (!text) {
+            return;
+        }
+
+        setTodoList(list => [{ id: randomNumber(1, 1e6), text }, ...list]);
+        setText(() => '');
+        inputRef.current.focus();
+    }
+
+    function handleDeleteClick(id) {
+        setTodoList(list => list.filter(item => item.id !== id));
+    }
+
+    // reikia <li> iskelti i atskira faila/komponenta
     return (
         <div className={style.todo}>
-            <p className={style}>CONTENT</p>
-            <button className={style.btn}>Click me</button>
+            <form onSubmit={handleFormSubmit} className={style.form}>
+                <input ref={inputRef} onChange={handleInputChange} value={text} className={style.input} type="text" />
+                <button className={style.btn} type="submit">Create</button>
+            </form>
+            <ul className={style.ul}>
+                {todoList.map(todo => (
+                    <li key={todo.id} className={style.li}>
+                        <p className={style.p}>{todo.text}</p>
+                        <div className={style.actions}>
+                            <button onClick={() => handleDeleteClick(todo.id)} className={style.btn}>Delete</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
