effe4c0234d99c19199181bf3e6555fc8baf03ce
Silvija96 <silvija.barkauskaite064@gmail.com>
2025-06-16 14:57:37 +0300

    routes

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import style from './Todo.module.css';
 import { randomNumber } from '../../lib/randomNumber';
 import { TodoItem } from './TodoItem';
@@ -6,9 +6,33 @@ import { TodoItem } from './TodoItem';
 export function Todo() {
     const storageKey = '54gr_todo';
 
-    const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem(storageKey)) ?? []);
+    const [todoList, setTodoList] = useState(null);
     const [text, setText] = useState('');
     const inputRef = useRef();
+
+    // komponentui uzsikrovus
+    useEffect(() => {
+        try {
+            const data = JSON.parse(localStorage.getItem(storageKey));
+            if (data) {
+                setTodoList(() => data);
+            } else {
+                setTodoList(() => []);
+            }
+        } catch (error) {
+            console.log(error);
+            setTodoList(() => []);
+        }
+    }, []);
+
+    // keiciantis konkreciam state
+    useEffect(() => {
+        if (todoList === null) {
+            return;
+        }
+
+        localStorage.setItem(storageKey, JSON.stringify(todoList));
+    }, [todoList]);
 
     function handleInputChange(e) {
         setText(e.target.value);
@@ -19,20 +43,21 @@ export function Todo() {
         if (!text) {
             return;
         }
+        console.log(todoList);
 
         setTodoList(list => [{ id: randomNumber(1, 1e6), text }, ...list]);
         setText(() => '');
         inputRef.current.focus();
-
-        localStorage.setItem(storageKey, JSON.stringify(todoList));
     }
 
     function handleDeleteClick(id) {
         setTodoList(list => list.filter(item => item.id !== id));
-        localStorage.setItem(storageKey, JSON.stringify(todoList));
     }
 
-    // reikia <li> iskelti i atskira faila/komponenta
+    function handleTextUpdate(id, newText) {
+        setTodoList(() => todoList.map(todo => todo.id === id ? { ...todo, text: newText } : todo));
+    }
+
     return (
         <div className={style.todo}>
             <form onSubmit={handleFormSubmit} className={style.form}>
@@ -40,7 +65,10 @@ export function Todo() {
                 <button className={style.btn} type="submit">Create</button>
             </form>
             <ul className={style.ul}>
-                {todoList.map(todo => <TodoItem key={todo.id} data={todo} handleDeleteClick={handleDeleteClick} />)}
+                {todoList && todoList.map(todo =>
+                    <TodoItem key={todo.id} data={todo}
+                        handleDeleteClick={handleDeleteClick}
+                        handleTextUpdate={handleTextUpdate} />)}
             </ul>
         </div>
     );
